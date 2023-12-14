8e7a15090d9c3349333ed0244aaa26eb81720eee
Personal1ty <personal1ty@mail.ru>
2023-12-14 16:50:48 +0300

    updated test_main_pages

--- test_main_page.py
+++ test_main_page.py
@@ -1,15 +1,28 @@
 from selenium.webdriver.common.by import By
 from .pages.main_page import MainPage
-
+from .pages.login_page import LoginPage
+import time
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
     page.open()                      # открываем страницу
-    page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
-
+    page.go_to_login_page()        # выполняем метод страницы — переходим на страницу логина
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
+    #time.sleep(5)
 def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+    #time.sleep(5)
+#def test_guest_should_see_login_page(browser):
+   # link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+   # page = LoginPage(browser, link)
+   # page.open()
+   # page.should_be_login_url()
+   # page.should_be_login_form()
+   # page.should_be_register_form()
+    #time.sleep(5) 
+  
