6c8810fe7a25428180c57f951ad825c4fa079abe
Personal1ty <personal1ty@mail.ru>
2023-12-24 16:01:27 +0300

    add update locators

--- pages/locators.py
+++ pages/locators.py
@@ -1,4 +1,6 @@
 from selenium.webdriver.common.by import By
+#from selenium.webdriver.support.ui import Select
+
 
 
 class MainPageLocators():
@@ -9,5 +11,16 @@ class LoginPageLocators():
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
 
 class ProductPageLocators():
-    ADD_BACKET = (By.CSS_SELECTOR, '[class="btn btn-lg btn-primary btn-add-to-basket"]')   
-   
+    ADD_BACKET = (By.CSS_SELECTOR, '[class="btn btn-lg btn-primary btn-add-to-basket"]')
+
+class LoginPageLocatorsBilbet():
+    CLICK_LOGIN_BUTTON = (By.CSS_SELECTOR, '[class="account__login account__btn"]')
+    INPUT_EMAIL = (By.CSS_SELECTOR, '[testid="login-email"]')
+    INPUT_PASS = (By.CSS_SELECTOR, '[testid="login-password"]')
+    CLICK_AUTHORIZATION_BUTTON = (By.CSS_SELECTOR, '[class="submit-button auth-content-form__submit"]')
+
+class RegisterPageLocatorsBilbet():
+    CLICK_REGISTER_BUTTON = (By.CSS_SELECTOR, '[class="account__register account__btn"]')
+    ACCOUNT_CURRENCY_ALL = (By.CSS_SELECTOR, '[class="currency-select__inner"]')
+
+    
